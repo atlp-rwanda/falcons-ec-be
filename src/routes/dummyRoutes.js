41b6525082dca54eff761665c99d515b94d107ff
Kirenga boris <Boris@Boris-MacBook-Pro.local>
2023-03-16 16:19:26 +0200

    ft():Add login route and auth middleware

--- src/routes/dummyRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getAllUsers } from '../controllers/dummyController';
-
-const dummyRoutes = express.Router();
-
-dummyRoutes.route('/').get(getAllUsers);
-
-export default dummyRoutes;
