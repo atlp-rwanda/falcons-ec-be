29af28eb32d6c5cbd622fb36411178302a80be3e
rwamugema <kylesjet1@gmail.com>
2023-04-25 17:20:46 +0200

    ch(setup/docker): setup docker for backend

--- src/database/models/product.js
+++ src/database/models/product.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { v4: uuidv4 } = require('uuid');
 const { Model } = require('sequelize');
 const User = require('./user');
--- src/utils/bcrypt.util.js
+++ src/utils/bcrypt.util.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 export class BcryptUtility {
   static async hashPassword(password) {
